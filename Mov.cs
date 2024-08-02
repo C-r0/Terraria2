44b9c44e4356e7033ac5182c1f21e06e2e567ab0
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-08-01 21:41:31 -0300

    Update Mov.cs

--- Mov.cs
+++ Mov.cs
@@ -17,25 +17,25 @@ public class Mov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movimento();
-        VirarLado();
+        Movimento(); // Chama o void Movimento()
+        VirarLado(); // Chama o void VirarLado()
     }
 
     void Movimento() // Movimenta o jogador
     {
-        float mx = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(mx, 0);
-        transform.Translate(movement * spd * Time.deltaTime);
+        float mx = Input.GetAxis("Horizontal"); // Colocar os dados do input axis horizontal dentro da variavel mx
+        Vector2 movement = new Vector2(mx, 0); // cria um vector2 chamado movement com os dados de (mx, 0)
+        transform.Translate(movement * spd * Time.deltaTime); // muda a posição do jogador de acordo com movemente * spd * time.deltatime
     }
-    void VirarLado()
+    void VirarLado() // Vira o lado do sprite
     {
-        if(Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.D)) // Verifica se clicou a tecla D
         {
-            transform.localScale = new Vector2(-1, 0);
+            transform.localScale = new Vector2(-1, 0); // Muda a escala para -1
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if(Input.GetKeyDown(KeyCode.A)) // Verifica se clicou a tecla A
         {
-            transform.localScale = new Vector2(1, 0);
+            transform.localScale = new Vector2(1, 0); // Muda a escala para 1
         }
     }
 }
